ae94f7eff05bcb63edb5d1e1c7a12efb5d7142da
bbagbbagn2 <pyoungh999@naver.com>
2024-01-28 04:20:54 +0900

    css 제거

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export default function Header() {
   const location = useLocation();
